dc57278168677f07d04fce84cfa8c836ff40f2c7
ebbmango <ebbmango@tuta.io>
2024-05-22 16:35:44 +0200

    visual feedback for current page

--- navigation/BottomTabsNavigator.js
+++ navigation/BottomTabsNavigator.js
@@ -6,6 +6,11 @@ import FoodsScreen from "../screens/FoodsScreen";
 import HomeScreen from "../screens/HomeScreen";
 import WeightScreen from "../screens/WeightScreen";
 
+// Icons
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 // Initializing navigator
 const Tab = createBottomTabNavigator();
 
@@ -13,9 +18,15 @@ const Tab = createBottomTabNavigator();
 export default function BottomTabsNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Foods" component={FoodsScreen} />
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Weight" component={WeightScreen} />
+      <Tab.Screen name="Foods" component={FoodsScreen} options={{
+        tabBarIcon: ({focused, color, size}) => <FontAwesome5 name="clipboard-list" size={24} color={focused ? "blue" : "black"} />
+      }}/>
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarIcon: ({focused, color, size}) => <MaterialCommunityIcons name="food-fork-drink" size={24} color={focused ? "blue" : "black"} />
+      }} />
+      <Tab.Screen name="Weight" component={WeightScreen} options={{
+        tabBarIcon: ({focused, color, size}) => <Ionicons name="scale" size={24} color={focused ? "blue" : "black"} />
+      }}/>
     </Tab.Navigator>
   );
 }
