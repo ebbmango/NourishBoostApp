2ce77621616ea4af91fae14d7dc45a4e827e56f3
ebbmango <ebbmango@tuta.io>
2024-05-27 10:50:10 +0200

    added 'add food' button to food page

--- screens/FoodsScreen.js
+++ screens/FoodsScreen.js
@@ -1,11 +1,52 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useSQLiteContext, addDatabaseChangeListener } from "expo-sqlite";
+import { Assets, Button, Colors, Icon, Image } from "react-native-ui-lib";
+import iconImage from "../assets/icons/plus-solid.svg";
+
+import PlusIcon from "../components/PlusIcon";
 
 export default function FoodsScreen() {
+  Assets.loadAssetsGroup("icons", {
+    icon1: require("../assets/icons/plus-solid.svg"),
+  });
+
+  const database = useSQLiteContext();
+
+  const getFoods = () => database.getAllSync("SELECT * FROM foods;");
+
+  const [foods, setFoods] = useState(getFoods());
+
+  addDatabaseChangeListener(() => {
+    setFoods(getFoods());
+  });
+
+  console.log(foods);
+
   return (
-    <View style={styles.container}>
-      <Text>Foods Screen</Text>
-    </View>
+    <>
+      <View
+        style={{
+          height: 50,
+          width: "100%",
+          backgroundColor: Colors.blue70,
+          display: "flex",
+          alignItems: "flex-end",
+          padding: 10,
+        }}
+      >
+        <Button
+          iconSource={() => {
+            return <PlusIcon color={Colors.white} />;
+          }}
+          round
+          style={{ width: 30, height: 30, padding: 6 }}
+        />
+      </View>
+      <View style={styles.container}>
+        <Text>Foods Screen</Text>
+      </View>
+    </>
   );
 }
 
