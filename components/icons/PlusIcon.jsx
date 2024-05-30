e8d02d68ed17201c51d680ff5e1f4ad573e5284e
ebbmango <ebbmango@tuta.io>
2024-05-30 20:29:36 +0200

    added icons: trash, edit, unit

---
+++ components/icons/PlusIcon.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+
+const PlusIcon = ({ color, ...props }) => (
+  <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" {...props}>
+    <Path
+      fill={color}
+      d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32v144H48c-17.7 0-32 14.3-32 32s14.3 32 32 32h144v144c0 17.7 14.3 32 32 32s32-14.3 32-32V288h144c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
+    />
+  </Svg>
+);
+
+export default PlusIcon;
+
+// License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc
+
