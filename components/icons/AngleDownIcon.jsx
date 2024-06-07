50c221c50d242f3bf2a55a082e68f97f97813f42
ebbmango <ebbmango@tuta.io>
2024-06-07 10:55:29 +0200

    created angle up and angle down icons

---
+++ components/icons/AngleDownIcon.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+
+const AngleDownIcon = ({ color, ...props }) => (
+  <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" {...props}>
+    <Path
+      fill={color}
+      d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
+    />
+  </Svg>
+);
+
+export default AngleDownIcon;
+
+// Icon:
+// Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com
+// License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc
+
+// Component:
+// https://react-svgr.com/playground/?native=true
