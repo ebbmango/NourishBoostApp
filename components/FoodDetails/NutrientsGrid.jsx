5081d039c1b4eda947238e1e608122465c209d79
ebbmango <ebbmango@tuta.io>
2024-06-10 21:21:25 +0200

    nutrients grid receives colors from items parameter

--- components/FoodDetails/NutrientsGrid.jsx
+++ components/FoodDetails/NutrientsGrid.jsx
@@ -1,10 +1,16 @@
 import { GridView, Text, View } from "react-native-ui-lib";
 import styles from "../../styles";
+import tweakStyles from "../../functions/tweakStyles";
 
 export default function NutrientsGrid({ items }) {
-  const renderCustomItem = ({ title, value, trailing }) => {
+  const renderCustomItem = ({ title, value, trailing, color }) => {
     return (
-      <View key={title} style={styles.foodDetailsScreen.nutrientsBoxStyle}>
+      <View
+        key={title}
+        style={tweakStyles(styles.foodDetailsScreen.nutrientsBoxStyle, {
+          backgroundColor: color,
+        })}
+      >
         <Text text70>{title}</Text>
         <Text text70BL>
           {value}
