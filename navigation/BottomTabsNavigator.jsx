d33e128fcafe48773c1bd77ee5bdeb7430aa2939
ebbmango <ebbmango@tuta.io>
2024-10-11 15:48:00 +0200

    deleted weightings and import/export data screens

--- navigation/BottomTabsNavigator.jsx
+++ navigation/BottomTabsNavigator.jsx
@@ -2,17 +2,9 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Colors } from "react-native-ui-lib";
 
-// Screens
-import WeightScreen from "../screens/WeightScreen";
-
 // Icons
 import UtensilsIcon from "../components/icons/UtensilsIcon";
-import ScaleIcon from "../components/icons/ScaleIcon";
 import MealsStackNavigator from "./MealsStackNavigator";
-import WeightStackNavigator from "./WeightStackNavigator";
-import ExportImportDataScreen from "../screens/ExportImportDataScreen";
-import FloppyDiskIcon from "../components/icons/FloppyDiskIcon";
-import ExportStackNavigator from "./ExportStackNavigator";
 
 // Initializing navigator
 const Tab = createBottomTabNavigator();
@@ -25,19 +17,6 @@ export default function BottomTabsNavigator() {
       initialRouteName="Diary"
     >
       <Tab.Screen
-        name="Data"
-        component={ExportStackNavigator}
-        options={{
-          tabBarShowLabel: false,
-          tabBarIcon: ({ focused }) => (
-            <FloppyDiskIcon
-              color={focused ? Colors.green30 : Colors.black}
-              style={{ width: 28, height: 28 }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
         name="Diary"
         component={MealsStackNavigator}
         options={{
@@ -46,19 +25,6 @@ export default function BottomTabsNavigator() {
             <UtensilsIcon
               color={focused ? Colors.green30 : Colors.black}
               style={{ width: 24, height: 24 }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Weight"
-        component={WeightStackNavigator}
-        options={{
-          tabBarShowLabel: false,
-          tabBarIcon: ({ focused }) => (
-            <ScaleIcon
-              color={focused ? Colors.green30 : Colors.black}
-              style={{ width: 26, height: 26 }}
             />
           ),
         }}
